8f13d16dae9543f6f39eadbd7ceda367ff8123d8
irfanshadikrishad <irfanshadikofficial@gmail.com>
2023-02-08 23:47:37 +0600

    views

--- src/script.js
+++ src/script.js
@@ -67,3 +67,17 @@ window.addEventListener("load", () => {
   document.getElementById("pre-loader").style.display = "none";
   document.getElementById("post-loader").style.display = "inline-block";
 });
+
+// Website Views
+function UpdateViews() {
+  fetch(
+    "https://api.countapi.xyz/update/irfanshadikrishad.github.io/visits/?amount=-1"
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data["value"]);
+      document.getElementById("views").innerHTML = data["value"];
+    });
+}
+
+UpdateViews();
